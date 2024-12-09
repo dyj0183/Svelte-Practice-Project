27167e4543e162ecbe625bed7f1f268fb0186149
Yu-Chun <dyj0183@byui.edu>
2024-12-09 15:11:19 -0700

    tried out  and

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+  let number = $state(0);
+  let count = $derived(number * 2);
+</script>
+
+<h1>Here is the number {number}</h1>
+<h2>Here is the count {count}</h2>
+<button on:click={() => number++}>Increment</button>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
