e7640ad652fd69826f31defa691912b31072314a
Yu-Chun <dyj0183@byui.edu>
2024-12-09 15:40:16 -0700

    bind user input value and use  as a debugging tool

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <script>
   let number = $state(0);
   let count = $derived(number * 2);
+  let userName = $state("");
+  // For debugging purpose
+  $inspect(userName)
 </script>
 
 <h1>Here is the number {number}</h1>
-<h2>Here is the count {count}</h2>
-<button on:click={() => number++}>Increment</button>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Here is the count (number * 2) {count}</h1>
+<button on:click={() => number++}>Click to increase the number</button>
+
+<h1>Your User Name</h1>
+<input type="text" bind:value={userName} />
+<p>Displaying the user name: {userName}</p>
